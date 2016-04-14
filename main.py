1c764355a8ea4520f967cbe9db85b45ee336e036
Leslie Dao <l.dao@hotmail.com>
2016-04-14 13:25:06 +0200

    manhattan functie

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # Team Paprikachips
 ##
 
+import math
+
 class Layer(object):
     def __init__(self):
         print "Hallo, wereld"
@@ -15,9 +17,35 @@ grid = [
     [0, 4, 0]
 ]
 
-wires = [
-    [{"n": 0, "e": 1, "w": 0, "s": 1, "u": 0, "d": 0}],
-    []
-]
+# wires = [
+#     [{"n": 0, "e": 1, "w": 0, "s": 1, "u": 0, "d": 0}],
+#     []
+# ]
 
-print grid[1][1]
+# Bereken de manhattan distance
+def manhattan(x, y):
+    # Zoek de coordinaten van x
+    for i in xrange(len(grid)):
+        if x in grid[i]:
+            x1 = i
+            break
+
+    for i in xrange(len(grid[x1])):
+        if x == grid[x1][i]:
+            x2 = i
+            break
+
+    # Zoek de coordinaten van y
+    for i in xrange(len(grid)):
+        if y in grid[i]:
+            y1 = i
+            break
+
+    for i in xrange(len(grid[y1])):
+        if y == grid[y1][i]:
+            y2 = i
+            break
+
+    return math.fabs(x1 - y1) + math.fabs(x2 - y2)
+
+print manhattan(1, 4)
